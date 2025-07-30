8b4c8ca26903c2090de3b6fa97229071e460102b
Xreej4n <sreejannath1@gmail.com>
2025-07-30 22:48:48 +0530

    Base Commit  Changes to be committed: 	new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/assets/cut-react-logo.png 	new file:   src/assets/react-logo.png 	new file:   src/assets/react.svg 	new file:   src/components/Main.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return(
+        <header>
+            <nav>
+                <img src="/src/assets/react.svg"/>
+                <p id="title">ReactFacts</p>
+            </nav>
+        </header>
+    )
+}
